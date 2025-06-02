2d63fd0c0ca90a48f2c4fd2bb374e07f49a0b4c6
Shreshtha-Ojha <148896860+Shreshtha-Ojha@users.noreply.github.com>
2025-06-02 19:59:01 +0530

    Create yt_background.js

---
+++ Extension - Frontend/yt_background.js
@@ -0,0 +1,2 @@
+agar right click kiya, toh usme "ask munna bhai" ka custom option aayega. if that is pressed, right side mein apna extension khol dena.
+video ka explanation, yaa transcript chaahiye ho toh (not sure about this feature - maybe later)
